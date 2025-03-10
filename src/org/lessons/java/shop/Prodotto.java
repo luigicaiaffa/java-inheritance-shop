1dde26c577014f80831b261d0ecccef5648a739a
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-10 11:23:15 +0100

    first commit

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,59 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Prodotto {
+
+    // # Variabili di istanza
+    protected int codice;
+    protected String nome;
+    protected String marca;
+    protected BigDecimal prezzo;
+    protected String iva;
+
+    // # Costruttore
+    public Prodotto(int codice, String nome, String marca, BigDecimal prezzo, String iva) {
+        this.codice = codice;
+        this.nome = nome;
+        this.marca = marca;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    // # Metodi
+    public int getCodice() {
+        return this.codice;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public BigDecimal getPrezzo() {
+        return this.prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getIva() {
+        return this.iva;
+    }
+
+    public void setIva(String iva) {
+        this.iva = iva;
+    }
+}
